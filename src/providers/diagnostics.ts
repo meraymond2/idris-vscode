0452699573a3f35777afda88e207c4ec0831fd3f
Michael Raymond <raymond.michael@gmail.com>
2021-07-10 19:30:14 +0100

    Compensate for bad .lidr offset

--- src/providers/diagnostics.ts
+++ src/providers/diagnostics.ts
@@ -1,15 +1,23 @@
+import { extname, isAbsolute } from "path"
 import * as vscode from "vscode"
 import { InfoReply } from "idris-ide-client"
 import { state } from "../state"
-import { isAbsolute } from "path"
 import { rmLocDesc } from "./diagnostic-utils"
 
 const warningToDiagnostic = (reply: InfoReply.Warning): vscode.Diagnostic => {
-  const { start, end, warning } = reply.err
-  const range = new vscode.Range(
-    new vscode.Position(start.line - 1, start.column - 1),
-    new vscode.Position(end.line - 1, end.column)
-  )
+  const { start, end, warning, filename } = reply.err
+  const isLidr = extname(filename) === ".lidr"
+  // .lidr positions are returned as if they’re normal Idris files, so they’re
+  // off by two, for the `> ` at the beginning.
+  const range = isLidr
+    ? new vscode.Range(
+        new vscode.Position(start.line - 1, start.column + 1),
+        new vscode.Position(end.line - 1, end.column + 2)
+      )
+    : new vscode.Range(
+        new vscode.Position(start.line - 1, start.column - 1),
+        new vscode.Position(end.line - 1, end.column)
+      )
   const editedWarning = rmLocDesc(warning)
   return new vscode.Diagnostic(range, editedWarning)
 }
@@ -19,13 +27,7 @@ export const handleWarning = (reply: InfoReply.Warning): void => {
   const filename = reply.err.filename
 
   // Idris2 sometimes uses relative file paths, which aren’t parsed into file URIs correctly on their own.
-  if (isAbsolute(filename)) {
-    const uri = vscode.Uri.file(filename)
-    const existing = diagnostics.get(uri) || []
-    diagnostics.set(uri, existing.concat(warningToDiagnostic(reply)))
-  } else if (idrisProcDir) {
-    const uri = vscode.Uri.file(idrisProcDir + "/" + filename)
-    const existing = diagnostics.get(uri) || []
-    diagnostics.set(uri, existing.concat(warningToDiagnostic(reply)))
-  }
+  const uri = isAbsolute(filename) ? vscode.Uri.file(filename) : vscode.Uri.file(idrisProcDir + "/" + filename)
+  const existing = diagnostics.get(uri) || []
+  diagnostics.set(uri, existing.concat(warningToDiagnostic(reply)))
 }
